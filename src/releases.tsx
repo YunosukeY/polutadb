097f274efd3ad5338029146181d30c9eab0b2f10
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2020-10-31 12:09:59 +0900

    add data

--- src/releases.tsx
+++ src/releases.tsx
@@ -6,6 +6,10 @@ export function Releases() {
       <h4>Release Notes</h4>
       重要度の低い情報は「細かい変更」にあります．<br />
 
+      <h5 className='release-date'>2020/10/30</h5>
+      <h6 className='release-content'>データの追加</h6>
+      ・『POLKA◆October◆LIVE◆2020 #ぽるうた 【ホロライブ/尾丸ポルカ】』分を追加しました<br />
+
       <h5 className='release-date'>2020/10/28</h5>
       <h6 className='release-content'>データの追加</h6>
       ・『酔いどれポルカのアイカツ！語り～アカペラもあるｙｐ～【ホロライブ/尾丸ポルカ】』分を追加しました<br />
