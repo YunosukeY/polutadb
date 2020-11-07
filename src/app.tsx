ab548e25642f71f0cf69e25128e53cde5906b9db
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2020-11-08 00:34:17 +0900

    githubioで動作しないバグを修正

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import { Header, Main, Footer } from './layout';
 
 export function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Main />
       <Footer />
