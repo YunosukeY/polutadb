450f534c3578fdfc57b68c7a3a270f4ee070c98c
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2021-03-01 13:39:57 +0900

    fix init timing

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Header, Main, Footer } from './layout';
+import M from 'materialize-css';
 
 export function App() {
+  useEffect(() => {
+    M.AutoInit();
+  });
+
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       {/* basenameはgithubioでの表示に必要 */}
