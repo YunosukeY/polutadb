aab1781a78fd5334bb2ce39ec0789f211b7f4173
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2021-03-13 22:25:55 +0900

    データのチェックを追加

---
+++ __test__/foreignKeyConstraint.test.ts
@@ -0,0 +1,41 @@
+import { songs, genres } from '../data/songs';
+import { artists } from '../data/artists';
+import { singings } from '../data/singings';
+import { videos, types } from '../data/videos';
+
+describe('外部キー代わりの添字が範囲内か', () => {
+  test('ジャンル->曲', () => {
+    for (const song of songs) {
+      expect(song.genreId).toBeGreaterThanOrEqual(0);
+      expect(song.genreId).toBeLessThan(genres.length);
+    }
+  });
+
+  test('アーティスト->曲', () => {
+    for (const song of songs) {
+      expect(song.artistId).toBeGreaterThanOrEqual(-1); // アーティストが空のものがある
+      expect(song.artistId).toBeLessThan(artists.length);
+    }
+  });
+
+  test('タイプ->動画', () => {
+    for (const video of videos) {
+      expect(video.typeId).toBeGreaterThanOrEqual(0);
+      expect(video.typeId).toBeLessThan(types.length);
+    }
+  });
+
+  test('曲->歌', () => {
+    for (const singing of singings) {
+      expect(singing.songId).toBeGreaterThanOrEqual(0);
+      expect(singing.songId).toBeLessThan(songs.length);
+    }
+  });
+
+  test('動画->歌', () => {
+    for (const singing of singings) {
+      expect(singing.videoId).toBeGreaterThanOrEqual(0);
+      expect(singing.videoId).toBeLessThan(videos.length);
+    }
+  });
+});
