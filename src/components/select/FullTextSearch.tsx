2ebd7aac509f6507ecbf9eff2fcd3973a4396c51
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2021-03-18 12:12:46 +0900

    fix
    - フォームをmaterial-uiに
    - cssの境界を修正

--- src/components/select/FullTextSearch.tsx
+++ src/components/select/FullTextSearch.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { useState } from 'react';
 import SearchIcon from '@material-ui/icons/Search';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 
 import { EachSelectProps } from './utils';
 
@@ -16,17 +18,15 @@ export default function FullTextSearch(props: EachSelectProps) {
   }
 
   return (
-    <div className='input-field'>
-      <SearchIcon color='action' className='material-icons prefix' style={{ fontSize: 28 }} />{' '}
-      {/* queryにtextをセット */}
-      <input
-        id='icon_prefix'
-        type='text'
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        onKeyDown={onKeyDown}
-      />
-      <label htmlFor='icon_prefix'>全文検索</label>
-    </div>
+    <>
+      <Grid container spacing={1} alignItems='flex-end'>
+        <Grid item>
+          <SearchIcon />
+        </Grid>
+        <Grid item>
+          <TextField label='全文検索' value={text} onChange={(e) => setText(e.target.value)} onKeyDown={onKeyDown} />
+        </Grid>
+      </Grid>
+    </>
   );
 }
