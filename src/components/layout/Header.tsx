24a360072c4ed47f83ea5bb320a2e8edc876695b
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2022-02-05 23:21:36 +0900

    feat: menu drawer化

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,20 +3,17 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 
 import Menu from './Menu';
-import MobileMenu from './MobileMenu';
 import { useRouter } from 'next/router';
 import { Button } from '@material-ui/core';
 
 export default function Header() {
-  const isMobile = screen.width < 1000;
-
   const router = useRouter();
 
   return (
     <header>
       <AppBar position='fixed' style={{ backgroundColor: '#ef5350', border: 0 }}>
         <Toolbar style={{ position: 'relative' }}>
-          <div style={{ flexGrow: 1 }}>{isMobile && <MobileMenu />}</div>
+          <div style={{ flexGrow: 1 }} />
           <Button
             onClick={() => router.push('/')}
             style={{
@@ -38,7 +35,7 @@ export default function Header() {
               PolutaDB
             </h4>
           </Button>
-          {!isMobile && <Menu />}
+          <Menu />
         </Toolbar>
       </AppBar>
       <Toolbar />
