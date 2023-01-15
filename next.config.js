c6f41ef884d29b2358ddbc6cdb0fecede099694f
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-01-15 15:43:45 +0900

    add api

--- next.config.js
+++ next.config.js
@@ -12,6 +12,12 @@ module.exports = withTM({
         destination: '/',
         permanent: true,
       },
+      {
+        source: '/api/data',
+        destination:
+          'https://script.google.com/macros/s/AKfycbw5PJ8ehZkqAgMuzs4NCLiZA9zNfqKY-ILVZAcoRBoL1sInlAKdntYS1yA4QttJHhTs/exec',
+        permanent: false,
+      },
     ];
   },
 });
