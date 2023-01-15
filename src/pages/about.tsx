9408b6e6a635472944dccd140f6f5b43f2900541
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-01-15 15:10:01 +0900

    aboutはfetch不要

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,24 +1,8 @@
-import { NextPage } from 'next';
 import * as React from 'react';
-import { useEffect } from 'react';
 
 import About from '../components/pane/About';
-import { fetchData } from '../data/data';
-import { Data } from '../data/interfaces';
-import { useInit } from '../lib/AppState';
 
-const Top: NextPage<Data> = (data) => {
-  const [isInit, init] = useInit();
-  useEffect(() => {
-    if (!isInit) init(data);
-  }, []);
-
+const Top = () => {
   return <About />;
 };
 export default Top;
-
-export async function getStaticProps(): Promise<{ props: Data }> {
-  return {
-    props: await fetchData(),
-  };
-}
