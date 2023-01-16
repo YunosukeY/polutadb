60e2e35c5f7cdd7d08571f4dd120a9bc1d1ab825
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-01-16 23:17:58 +0900

    Revert "archived"
    This reverts commit 4089f91e41734aafded786aa1303b9e76249e57d.

--- src/components/layout/KeyVisual.tsx
+++ src/components/layout/KeyVisual.tsx
@@ -1,21 +1,50 @@
 import * as React from 'react';
-import { Alert } from '@material-ui/lab';
-import { makeStyles } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import Slider, { Settings } from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const useStyles = makeStyles(() => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-}));
+import Deformed from './Deformed';
+import PickUpThumbnail from './PickUpThumbnail';
+import { Singing } from '../../data/interfaces';
 
 export default function KeyVisual() {
-  const classes = useStyles();
+  const router = useRouter();
+  const isTop = () => {
+    return router.pathname === '/';
+  };
 
   return (
-    <Alert severity='warning' className={classes.root}>
-      このサイトの更新は終了しました
-    </Alert>
+    <>
+      {isTop() && <PickUp />}
+      {!isTop() && <Deformed />}
+    </>
+  );
+}
+
+function PickUp() {
+  const settings: Settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: true,
+    arrows: false,
+  };
+  return (
+    <div style={{ paddingBottom: 20 }}>
+      <Slider {...settings}>
+        <PickUpThumbnail id='LQ_eazT56FA' singing={new Singing(0, 27, 0, 0)} />
+        <PickUpThumbnail id='TGJ9-1LWFtE' singing={new Singing(0, 64, 0, 0)} />
+        <PickUpThumbnail id='vbhgQ_C_jaM' singing={new Singing(0, 70, 0, 0)} />
+        <PickUpThumbnail id='kdYAZZ5SOFk' singing={new Singing(0, 79, 0, 0)} />
+        <PickUpThumbnail id='0bo6MVQxY6Y' singing={new Singing(0, 128, 0, 0)} />
+        <PickUpThumbnail id='zHD5MCUezVo' singing={new Singing(0, 129, 0, 0)} />
+        <PickUpThumbnail id='uefGU_oOCdk' singing={new Singing(0, 130, 0, 0)} />
+      </Slider>
+    </div>
   );
 }
