215d9a2b683749a4417307e4657038a2afc31ed4
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-01-16 23:12:27 +0900

    fix: 検索条件変えてページが1にならないを修正

--- src/components/select/utils.ts
+++ src/components/select/utils.ts
@@ -1,5 +1,7 @@
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { useRouter } from 'next/router';
+import { useSetRecoilState } from 'recoil';
+import { appState } from '../../lib/AppState';
 
 import { Query } from '../../lib/query';
 
@@ -18,9 +20,14 @@ export const useStyles = makeStyles((theme: Theme) =>
 export const useOnChange = (onChange: (...e: any[]) => void, qUpdater: (q: Query, value: any) => void) => {
   const router = useRouter();
   const query = new Query(router.query);
+  const setState = useSetRecoilState(appState);
+  const initPage = () => {
+    setState((state) => ({ ...state, pagenum: 1 }));
+  };
   return (value: any) => {
     onChange(value);
     qUpdater(query, value);
+    initPage();
     router.push(`/?${query}`, undefined, { scroll: false });
   };
 };
