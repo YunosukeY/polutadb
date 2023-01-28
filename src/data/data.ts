d781d5a839af715c468ecc38424adbead5dbef1c
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-01-28 10:45:05 +0900

    検索結果の並び順修正

--- src/data/data.ts
+++ src/data/data.ts
@@ -4,5 +4,15 @@ export const fetchData = async () => {
   const deployId = 'AKfycbw5PJ8ehZkqAgMuzs4NCLiZA9zNfqKY-ILVZAcoRBoL1sInlAKdntYS1yA4QttJHhTs';
   const res = await fetch(`https://script.google.com/macros/s/${deployId}/exec`);
   const data: Data = await res.json();
+
+  const m = new Map(data.videos.map((v) => [v.title, v.date]));
+  data.singings.sort((a, b) => {
+    const aDate = m.get(a.video)!;
+    const bDate = m.get(b.video)!;
+    if (aDate > bDate) return 1;
+    else if (aDate < bDate) return -1;
+    else return 0;
+  });
+
   return data;
 };
