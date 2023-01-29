1cb74b6f16796deaf72885a27b137d56b46c0473
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-01-29 20:11:23 +0900

    amcharts4固有の処理を削除

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-const withTM = require('next-transpile-modules')([
-  '@amcharts/amcharts4/core',
-  '@amcharts/amcharts4/charts',
-  '@amcharts/amcharts4/themes/animated',
-]); // pass the modules you would like to see transpiled
-
-module.exports = withTM({
+module.exports = {
   async redirects() {
     return [
       {
@@ -20,4 +14,4 @@ module.exports = withTM({
       },
     ];
   },
-});
+};
