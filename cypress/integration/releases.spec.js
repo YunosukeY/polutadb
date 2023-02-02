901965b7271142e5e576bafbdb33c4b3213d3efb
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-02-02 19:16:17 +0900

    remove cypress

--- cypress/integration/releases.spec.js
+++
@@ -1,7 +0,0 @@
-describe('リリースノート', () => {
-  it('sentence', () => {
-    cy.visit('/releases');
-    cy.contains('Latest Release Note');
-    cy.contains('Old Release Notes');
-  });
-});
--- cypress/integration/search.spec.js
+++
@@ -1,9 +0,0 @@
-// TODO: 検索結果のテスト
-
-describe('検索', () => {
-  it('sentence', () => {
-    cy.visit('/');
-    cy.contains('Search'); // TODO
-    cy.contains('Result');
-  });
-});
