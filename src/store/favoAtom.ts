7815cbedf7572f2fcdd4265d10f074919a2f719d
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-02-10 21:01:12 +0900

    yarn format

--- src/store/favoAtom.ts
+++ src/store/favoAtom.ts
@@ -7,10 +7,15 @@ export const favoAtom = atom<Map<number, boolean>>({
 
 export const favoSelector = selectorFamily<boolean, { singingId: number }>({
   key: `${favoAtom.key}/favoSelector`,
-  get: ({ singingId }) => ({ get }) => Boolean(get(favoAtom).get(singingId)),
-  set: ({ singingId }) => ({ set }, newValue) => {
-    newValue instanceof DefaultValue
-      ? set(favoAtom, newValue)
-      : set(favoAtom, (prev) => new Map(prev.set(singingId, newValue)));
-  },
+  get:
+    ({ singingId }) =>
+    ({ get }) =>
+      Boolean(get(favoAtom).get(singingId)),
+  set:
+    ({ singingId }) =>
+    ({ set }, newValue) => {
+      newValue instanceof DefaultValue
+        ? set(favoAtom, newValue)
+        : set(favoAtom, (prev) => new Map(prev.set(singingId, newValue)));
+    },
 });
