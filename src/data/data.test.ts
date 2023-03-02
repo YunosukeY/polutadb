a09622710b079d229e1d69ae282004a5a8c3c671
Yunosuke Yamada <39757050+YunosukeY@users.noreply.github.com>
2023-03-02 20:11:35 +0900

    test: Data consistency (#120)
    * chore: yarn add -D jest ts-jest @types/jest
    
    * chore: npx syncyarnlock -s
    
    * chore: yarn install
    
    * chore: add jest.config.js
    
    - https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler
    
    * chore: comment out
    
    * chore: eslint-plugin-jest
    
    * test: data consistency
    
    * ci: yarn test
    
    * refactor

---
+++ src/data/data.test.ts
@@ -0,0 +1,56 @@
+import { fetchData } from './data';
+import { Data } from './types';
+
+describe('data consistency', () => {
+  let data: Data;
+  beforeAll(async () => {
+    data = await fetchData();
+  });
+
+  describe('unique constraint', () => {
+    test('artists.name', () => {
+      data.artists.forEach((a) => {
+        const same = data.artists.filter((a2) => a2.name === a.name);
+        expect(same).toHaveLength(1);
+      });
+    });
+
+    test('songs.title', () => {
+      data.songs.forEach((s) => {
+        const same = data.songs.filter((s2) => s2.title === s.title);
+        expect(same).toHaveLength(1);
+      });
+    });
+  });
+
+  describe('foreign key constraint', () => {
+    test('types.name -> videos.type', () => {
+      data.videos.forEach((v) => {
+        const ref = data.types.find((t) => t.name === v.type);
+        expect(ref).not.toBeUndefined();
+      });
+    });
+
+    test('artists.name -> song.artist', () => {
+      data.songs.forEach((s) => {
+        if (!s.artist) return;
+        const ref = data.artists.find((a) => a.name === s.artist);
+        expect(ref).not.toBeUndefined();
+      });
+    });
+
+    test('videos.title -> singings.video', () => {
+      data.singings.forEach((s) => {
+        const ref = data.videos.find((v) => v.title === s.video);
+        expect(ref).not.toBeUndefined();
+      });
+    });
+
+    test('songs.title -> singings.song', () => {
+      data.singings.forEach((s) => {
+        const ref = data.songs.find((song) => song.title === s.song);
+        expect(ref).not.toBeUndefined();
+      });
+    });
+  });
+});
