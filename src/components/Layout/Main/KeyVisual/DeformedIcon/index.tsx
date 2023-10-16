3761bc599936c18fb727fa228702b1bcc047e62a
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-10-16 20:23:48 +0900

    fix: Image elements do not have [alt] attributes

--- src/components/Layout/Main/KeyVisual/DeformedIcon/index.tsx
+++ src/components/Layout/Main/KeyVisual/DeformedIcon/index.tsx
@@ -3,19 +3,12 @@ import * as React from 'react';
 export default function DeformedIcon() {
   return (
     <div style={{ backgroundColor: '#fff', textAlign: 'center' }}>
-      {screen.width <= 500 && (
-        <img src='/card.png' alt='OmaruPolka' title='OmaruPolka' style={{ width: screen.width }} />
-      )}
+      {screen.width <= 500 && <img src='/card.png' title='OmaruPolka' style={{ width: screen.width }} />}
       {500 < screen.width && screen.width <= 760 && (
-        <img src='/card-smaller-credit.png' alt='OmaruPolka' title='OmaruPolka' style={{ width: screen.width }} />
+        <img src='/card-smaller-credit.png' title='OmaruPolka' style={{ width: screen.width }} />
       )}
       {760 < screen.width && (
-        <img
-          src='/card-smaller-credit.png'
-          alt='OmaruPolka'
-          title='OmaruPolka'
-          style={{ height: '400px', margin: '0 auto' }}
-        />
+        <img src='/card-smaller-credit.png' title='OmaruPolka' style={{ height: '400px', margin: '0 auto' }} />
       )}
     </div>
   );
