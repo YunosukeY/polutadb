c56859335981ad30b25c4a4bf6807f5fe7a333b5
Yunosuke Yamada <39757050+YunosukeY@users.noreply.github.com>
2024-04-21 20:35:25 +0900

    release: 20240421 (#298)

--- src/components/Releases/index.tsx
+++ src/components/Releases/index.tsx
@@ -75,6 +75,11 @@ const OldReleases: FCWithChildren = ({ children }) => {
 
 const ReleaseArray: React.ReactNode[] = [
   <>
+    <Date date='2024/04/21' />
+    <AddData />
+    <AddVideo videoId={225} />
+  </>,
+  <>
     <Date date='2024/04/14' />
     <AddData />
     <AddVideo videoId={223} />
