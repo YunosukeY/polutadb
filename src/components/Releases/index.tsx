c3d31fe57eacd78713190fd3d53c0ed88c16cc77
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2025-09-27 08:35:00 +0900

    add data

--- src/components/Releases/index.tsx
+++ src/components/Releases/index.tsx
@@ -74,6 +74,11 @@ const OldReleases: FCWithChildren = ({ children }) => {
 
 const ReleaseArray: React.ReactNode[] = [
   <>
+    <Date date='2025/09/27' />
+    <AddData />
+    <AddVideo videoId={287} />
+  </>,
+  <>
     <Date date='2025/09/01' />
     <AddData />
     <AddVideo videoId={285} />
